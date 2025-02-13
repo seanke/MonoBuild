594ce655e8ac206aab8f10963eac39956b4c3254
Sean Kelly <seankelly@outlook.co.nz>
2025-02-13 20:30:09 +1300

    update

--- Engine/Constants.cs
+++ Engine/Constants.cs
@@ -4,6 +4,6 @@ namespace Engine;
 
 internal static class Constants
 {
-    internal const float BuildHeightUnitMeterRatio = -1 / 256f; // Scale factor from Duke3D to standard 3D
-    internal const float BuildWidthUnitMeterRatio = 1 / 16f; // Scale factor from Duke3D to standard 3D
+    internal const float BuildHeightUnitMeterRatio = -1 / 256f; // Scale factor from Build to standard 3D
+    internal const float BuildWidthUnitMeterRatio = 1 / 16f; // Scale factor from Build to standard 3D
 }
